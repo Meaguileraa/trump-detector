382781e7c62fbe2203f9639802bbf5ceefe9875b
Marissa Aguilera <marissaguilera@yahoo.com>
2020-11-06 14:26:52 -0800

    instructions

---
+++ solution.py
@@ -0,0 +1,15 @@
+
+
+# We all love the future president (or Führer or duce or sōtō as he could find 
+# them more fitting) donald trump, but we might fear that some of his many fans 
+# like John Miller or John Barron are not making him justice, sounding too much 
+# like their (and our as well, of course!) hero and thus risking to compromise him.
+
+# For this reason we need to create a function to detect the original and unique 
+# rythm of our beloved leader, typically having a lot of extra vowels, all ready 
+# to fight the estabilishment.
+
+# The index is calculated based on how many vowels are repeated more than 
+# once in a row and dividing them by the total number of vowels a petty enemy 
+# of America would use.
+
